82c64b7f4d55fccdd4fe49f1cfbecd95a66039b1
Jason Dansie <jasondansie@gmail.com>
2023-01-12 12:34:33 +0200

    adds simple form logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [userName, setUserName] = useState('');
+  const [updated, setUpdated] = useState(userName);
 
-  const submitHadler = () => {
+  const handleChange = (e) => {
+    setUserName(e.target.value);
+  }
+
+  const submitHandler = () => {
     console.log("button clicked");
+    setUpdated(userName);
   }
 
   return (
     <div>
-      <button onClick={submitHadler()}>submit</button>
+      <input type="text"
+        id="username"
+        name='username'
+        onChange={handleChange}
+        value={userName}
+      />
+      <h2>Username: {userName}</h2>
+      <h2>Updated: {updated}</h2>
+      <button onClick={submitHandler}>submit</button>
     </div>
   );
 }
